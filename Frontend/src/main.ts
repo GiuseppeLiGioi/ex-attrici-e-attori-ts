253d9fff45e1f99a59496e08683a07288615fe22
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-06-14 18:45:29 +0200

    added. backend

---
+++ Frontend/src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+/*
+Nota: a differenza di quanto visto finora negli esempi, per accedere all'API utilizzare l'url base:
+http://localhost:3333
+al posto di:
+https://freetestapi.com/api/v1
+Ad esempio:
+http://localhost:3333/users
+per chiamare l'endpoint /users
+Clicca qui per la guida su come installare il Server API Locale!
+*/
