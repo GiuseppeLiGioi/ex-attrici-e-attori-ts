2150ab59c0fa0600367c8d91dfa53ab7e2515cea
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-06-14 18:50:01 +0200

    added. milestone 1

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,11 +1,24 @@
 import './style.css'
 /*
-Nota: a differenza di quanto visto finora negli esempi, per accedere all'API utilizzare l'url base:
-http://localhost:3333
-al posto di:
-https://freetestapi.com/api/v1
-Ad esempio:
-http://localhost:3333/users
-per chiamare l'endpoint /users
-Clicca qui per la guida su come installare il Server API Locale!
+📌 Milestone 1
+Crea un type alias Person per rappresentare una persona generica.
+
+Il tipo deve includere le seguenti proprietà:
+
+id: numero identificativo, non modificabile
+name: nome completo, stringa non modificabile
+birth_year: anno di nascita, numero
+death_year: anno di morte, numero opzionale
+biography: breve biografia, stringa
+image: URL dell'immagine, stringa
 */
+
+type Person = {
+readonly id : number,
+readonly name : string,
+birth_year : number,
+death_year? : number,
+biography: string,
+image: string
+
+}
